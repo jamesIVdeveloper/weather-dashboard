5a537bcfb0245d2bc4ccfe788bca16bfca95b62c
James Murphy <jamesivdeveloper@gmail.com>
2022-01-27 14:45:29 -0600

    handle form submit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,34 @@
+var currentWeatherEl = document.querySelector("#current-weather");
 var cityInputEl = document.querySelector("#city-name");
+var cityFormEl = document.querySelector("#city-form");
+
+// handle form submit
+var formSubmitHandler = function (event) {
+  event.preventDefault();
+
+  console.log("working");
+
+  var city = cityInputEl.value.trim();
+
+  if (city) {
+    getCurrentWeather(city);
+    cityInputEl.value = "";
+  } else {
+    alert("Please enter a City");
+  }
+};
+
+// add event listener for form submit
+cityFormEl.addEventListener("submit", formSubmitHandler);
 
 // fetch current weather data
 var getCurrentWeather = function (city) {
   var apiKey = "6f96b0c5c7809d512ddbab8d202413e7";
 
   var apiUrl =
-    "https://api.openweathermap.org/data/2.5/weather?q=nashville&appid=" +
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    city +
+    "&appid=" +
     apiKey;
 
   fetch(apiUrl)
@@ -23,4 +46,4 @@ var getCurrentWeather = function (city) {
     });
 };
 
-getCurrentWeather();
+getCurrentWeather("nashville");
