9d452fdbb488773fe6ed464fa44d6e121a2a0e88
James Murphy <jamesivdeveloper@gmail.com>
2022-01-28 15:40:34 -0600

    display five day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ var cityFormEl = document.querySelector("#city-form");
 var currentTitleEl = document.querySelector("#city-date-icon");
 var weatherDataEl = document.querySelector("#weather-data");
 var currentDataList = document.querySelector("#current-data-list");
+var fiveDayEl = document.querySelector("#five-day-forecast");
 
 // handle form submit
 var formSubmitHandler = function (event) {
@@ -13,6 +14,7 @@ var formSubmitHandler = function (event) {
 
   if (city) {
     getCurrentWeather(city);
+    getFiveDayForecast(city);
     cityInputEl.value = "";
   } else {
     alert("Please enter a City");
@@ -68,4 +70,73 @@ var displayCurrentWeather = function (data, searchTerm) {
   currentDataList.appendChild(humidityEl);
   currentDataList.appendChild(windSpeedEl);
   // currentDataList.appendChild();
+};
+
+// fetch five day forecast
+var getFiveDayForecast = function (city) {
+  apiKey = "6f96b0c5c7809d512ddbab8d202413e7";
+  apiUrl =
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
+    city +
+    "&units=imperial" +
+    "&appid=" +
+    apiKey;
+
+  fetch(apiUrl)
+    .then(function (response) {
+      if (response.ok) {
+        response.json().then(function (data) {
+          console.log(data);
+          displayFiveDayForecast(data);
+        });
+      } else {
+        alert("Error: City not found");
+      }
+    })
+    .catch(function (error) {
+      alert("Unable to connect to Open Weather");
+    });
+};
+
+// display five day forecast
+var displayFiveDayForecast = function (data) {
+  console.log("working");
+
+  // loop through list
+  for (var i = 0; i < data.list.length; i += 8) {
+    // format data
+    var dateEl = data.list[i].dt_txt;
+    var iconEl = data.list[i].weather[0].icon;
+    var tempEl = data.list[i].main.temp;
+    var windSpeedEl = data.list[i].wind.speed;
+    var humidityEl = data.list[i].main.humidity;
+
+    // create a container for each day
+    var dayEl = document.createElement("div");
+
+    // create h4 for date
+    var dayDateEl = document.createElement("h4");
+    dayDateEl.textContent = dateEl;
+
+    // create p for icon, temp, wind speed, humidity
+    var dayIconEl = document.createElement("p");
+    dayIconEl.textContent = iconEl;
+    var dayTempEl = document.createElement("p");
+    dayTempEl.textContent = "Temperature: " + tempEl + " Fahrenheit";
+    var dayWindSpeedEl = document.createElement("p");
+    dayWindSpeedEl.textContent = "Wind Speed: " + windSpeedEl + "MPH";
+    var dayHumidityEl = document.createElement("p");
+    dayHumidityEl.textContent = "Humidity: " + humidityEl + "%";
+
+    // append to dayEl
+    dayEl.appendChild(dayDateEl);
+    dayEl.appendChild(dayIconEl);
+    dayEl.appendChild(dayTempEl);
+    dayEl.appendChild(dayWindSpeedEl);
+    dayEl.appendChild(dayHumidityEl);
+
+    // append to fiveDayEl
+    fiveDayEl.appendChild(dayEl);
+  }
+  console.log(dateEl, iconEl, tempEl, windSpeedEl, humidityEl);
 };
